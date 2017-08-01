9da9f3ee57fc051d9fe1f9d1749ca8e3c554d949
Tetsuya Mori <monry84@gmail.com>
2017-08-01 17:19:02 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Singleton にコンポーネントにアクセスするための機能を提供する
+/// </summary>
+/// <typeparam name="T">コンポーネントの型</typeparam>
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
+
+	/// <summary>
+	/// 自身のインスタンスの実体
+	/// </summary>
+	private static T instance;
+
+	/// <summary>
+	/// 自身のインスタンス
+	/// </summary>
+	public static T Instance {
+		get {
+			if (instance == default(T)) {
+				instance = Permanent.Add<T>();
+			}
+			return instance;
+		}
+		private set {
+			instance = value;
+		}
+	}
+
+	/// <summary>
+	/// Unity lifecycle: Awake
+	/// </summary>
+	/// <remarks>Singleton 化のために、既にインスタンスが存在していたら自殺</remarks>
+	public virtual void Awake() {
+		if (FindObjectOfType<T>() != null && FindObjectOfType<T>().gameObject != this.gameObject) {
+			Destroy(this);
+		} else {
+			Instance = (T)this;
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# What?
+
+* 任意の MonoBehaviour に Singleton にアクセス出来るフィールドを提供します
+
+# Why?
+
+* シーンを跨いでアクセスしたりするコンポーネントが少なからず存在していたので実装しました
+
+# Install
+
+```shell
+$ npm install @umm/singleton_monobehaviour
+```
+
+# Usage
+
+```csharp
+public class Hoge : SingletonMonoBehaviour<Hoge> {
+
+    public string Fuga() {
+        return "Fuga!!!";
+    }
+
+}
+
+public class Fuga {
+
+    public void Piyo() {
+        Debug.Log(Hoge.Instance.Fuga());
+    }
+
+}
+```
+
+# License
+
+Copyright (c) 2017 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
